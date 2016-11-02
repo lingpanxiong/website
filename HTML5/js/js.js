00b2adc8eb5ba3e9beec14e02bc1990bea23bafe
iront <539966608@qq.com>
2016-11-02 22:11:44 +0800

    下拉菜单完成
    下拉菜单完成

--- HTML5/js/js.js
+++ HTML5/js/js.js
@@ -97,24 +97,13 @@ $(document).ready
 
 		var canvas=document.getElementById('divjs').children[0],
 			context=canvas.getContext('2d');
-			gradient=context.createLinearGradient(0,0,canvas.width,0);
-
-		gradient.addColorStop(0,'blue');
-		gradient.addColorStop(0.25,'white');
-		gradient.addColorStop(0.5,'purple');
-		gradient.addColorStop(0.75,'red');
-		gradient.addColorStop(1,'yellow');
 
 		context.lineJoin='round';
-		context.lineWidth=5;
+		context.lineWidth=3;
 
 		context.strokeStyle;'goldenrod';
-		context.fillStyle='white';
-		context.rect(0,0,canvas.width,canvas.height);
-
 		context.strokeRect(50,50,100,100);
-		context.fillRect(49,49,99,99);
-
+		context.strokeRect(75,75,100,100);
 	}
 
 );
@@ -129,14 +118,118 @@ function()
 	contextc.moveTo(200,200);
 
 	contextc.beginPath();
-	contextc.arc(indexcanvas.width/2,indexcanvas.height/2,100,Math.PI*0,Math.PI*2,false);
+	contextc.arc(indexcanvas.width/2,indexcanvas.height/2,60,Math.PI*0,Math.PI*2,false);
 
 	contextc.stroke();
 	contextc.beginPath();
-	contextc.arc(indexcanvas.width/2+50,indexcanvas.height/2+50,100,Math.PI*0,Math.PI*2,false);
-	
+	contextc.arc(indexcanvas.width/2+50,indexcanvas.height/2+50,60,Math.PI*0,Math.PI*2,false);
 	contextc.stroke();
 
 }
-
 )
+
+$(document).ready
+(
+	function()
+	{
+		$(".index").mouseover
+		(
+			function()
+			{
+
+				$(".index").css("height":"150%","background-color":"purple");
+			}
+			
+		);
+
+		$(".index").mouseout
+		(
+			function()
+			{
+				
+				$(".index").css("height","20%");
+			}
+			
+		);
+
+	}
+);
+
+$(document).ready
+(
+	function()
+	{
+		$(".about").mouseover
+		(
+			function()
+			{
+
+				$(".about").css("height","150%");
+			}
+			
+		);
+		$(".about").mouseout
+		(
+			function()
+			{
+
+				$(".about").css("height","20%");
+			}
+			
+		);
+
+	}
+);
+
+$(document).ready
+(
+	function()
+	{
+		$(".hr").mouseover
+		(
+			function()
+			{
+
+				$(".hr").css("height","150%");
+			}
+			
+		);
+		$(".hr").mouseout
+		(
+			function()
+			{
+
+				$(".hr").css("height","20%");
+			}
+			
+		);
+
+	}
+);
+
+$(document).ready
+(
+	function()
+	{
+		$(".map").mouseover
+		(
+			function()
+			{
+
+				$(".map").css("height","150%");
+			}
+			
+		);
+		$(".map").mouseout
+		(	
+			function()
+			{	
+				$(".map").css("height","20%");
+			}
+		
+		);
+
+	}
+);
+
+
