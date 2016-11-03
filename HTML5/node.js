30960de6b76ae502659c23543511de85db57d49d
iront <539966608@qq.com>
2016-11-04 04:27:16 +0800

    add node.js server
    add node.js server

--- HTML5/node.js
+++ HTML5/node.js
@@ -1,8 +1,15 @@
-var http=require('http')
+var http=require('http');
+var url=require('url');
 
-http.createServer(function (require,response) {
+http.createServer(function onRequest(request,response) {
+	var pathname=url.parse(request.url).pathname;
+	console.log("Request for"+pathname+"Received.");
 
 	response.writeHead(200,{'Content-Type':'text/plain'});
-	response.end('hello world\n');
+	response.write(pathname);
+	response.end();
+
+	
+
 }).listen(8888);
 console.log('server running at http://127.0.0.1:88888/');
